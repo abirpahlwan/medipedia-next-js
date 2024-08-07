7058a312197029ccf0b755294d14d24c7369982d
abirpahlwan <abir978@gmail.com>
2024-08-08 05:33:42 +0600

    clean git

---
+++ README.md
@@ -0,0 +1,12 @@
+# Medipedia
+
+Medicine reference for students, doctors, pharmacists, nurses, and other healthcare professionals.
+
+## Features
+
+- Generics
+- Brands
+- Indications
+- Pharmaceuticals
+- Pharmacology
+- Therapeutic Classes
---
+++ config/site.ts
@@ -0,0 +1,17 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Medipedia",
+  description:
+    "Medicine reference for students, doctors, pharmacists, nurses, and other healthcare professionals.",
+  mainNav: [
+    {
+      
+    },
+  ],
+  links: {
+    facebook: "https://facebook.com/abirpahlwan",
+    twitter: "https://twitter.com/abirpahlwan",
+    github: "https://github.com/abirpahlwan",
+  },
+}
