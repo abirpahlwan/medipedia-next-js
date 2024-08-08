e264b0ee0db73630db8d7eb169a9b7124b8422e2
abirpahlwan <abirpahlwan@gmail.com>
2024-08-08 07:45:16 +0600

    large search bar

--- app/sections/feature-section.tsx
+++ app/sections/feature-section.tsx
@@ -4,7 +4,7 @@ import {FeatureCardCenter} from "@/components/feature-card-center";
 
 export function FeatureSection() {
 	return (
-		<section className="container mx-auto grid grid-cols-1 gap-6 bg-slate-50 px-12 py-12 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3">
+		<section className="container mx-auto grid grid-cols-1 gap-6 bg-slate-50 px-12 py-12 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 dark:bg-transparent">
 			<FeatureCardCenter />
 			<FeatureCardCenter />
 			<FeatureCardCenter />
--- components/search-bar.tsx
+++ components/search-bar.tsx
@@ -6,7 +6,7 @@ export function SearchBar(){
 		<section className="flex justify-center py-8">
 			<div className="relative w-full max-w-5xl">
 				<SearchIcon className="absolute left-3 top-1/2 size-5 -translate-y-1/2 text-muted-foreground"/>
-				<Input className="w-full rounded-md bg-muted px-10 focus:outline-none focus:ring-1 focus:ring-primary"
+				<Input className="w-full rounded-md bg-muted px-10 py-6 text-lg focus:outline-none focus:ring-1 focus:ring-primary"
 					   type="search" placeholder="Search anything..."
 				/>
 			</div>
