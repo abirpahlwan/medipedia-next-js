0a2e63529b641a7d69def06a1de87eade4a20d76
abirpahlwan <abirpahlwan@gmail.com>
2024-08-08 07:04:44 +0600

    search bar

--- components/search-bar.tsx
+++ components/search-bar.tsx
@@ -6,10 +6,8 @@ export function SearchBar(){
 		<section className="flex justify-center py-8">
 			<div className="relative w-full max-w-5xl">
 				<SearchIcon className="absolute left-3 top-1/2 size-5 -translate-y-1/2 text-muted-foreground"/>
-				<Input
-					type="search"
-					placeholder="Search anything..."
-					className="w-full rounded-md bg-muted px-10 focus:outline-none focus:ring-1 focus:ring-primary"
+				<Input className="w-full rounded-md bg-muted px-10 focus:outline-none focus:ring-1 focus:ring-primary"
+					   type="search" placeholder="Search anything..."
 				/>
 			</div>
 		</section>
--- components/site-header.tsx
+++ components/site-header.tsx
@@ -11,34 +11,19 @@ export function SiteHeader() {
 		<header className="sticky top-0 z-40 w-full border-b bg-background">
 			<div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
 				<MainNav items={siteConfig.mainNav}/>
-				<div className="flex flex-1 items-center justify-end space-x-4">
+				{/*<div className="flex grow justify-center">*/}
+				{/*	<SearchBar/>*/}
+				{/*</div>*/}
+				<div className="flex items-center justify-end space-x-4">
 					<nav className="flex items-center space-x-1">
-						<Link
-							href={siteConfig.links.github}
-							target="_blank"
-							rel="noreferrer"
-						>
-							<div
-								className={buttonVariants({
-									size: "icon",
-									variant: "ghost",
-								})}
-							>
+						<Link href={siteConfig.links.github} target="_blank" rel="noreferrer">
+							<div className={buttonVariants({size: "icon", variant: "ghost"})}>
 								<Icons.gitHub className="size-5"/>
 								<span className="sr-only">GitHub</span>
 							</div>
 						</Link>
-						<Link
-							href={siteConfig.links.twitter}
-							target="_blank"
-							rel="noreferrer"
-						>
-							<div
-								className={buttonVariants({
-									size: "icon",
-									variant: "ghost",
-								})}
-							>
+						<Link href={siteConfig.links.twitter} target="_blank" rel="noreferrer">
+							<div className={buttonVariants({size: "icon", variant: "ghost"})}>
 								<Icons.twitter className="size-5 fill-current"/>
 								<span className="sr-only">Twitter</span>
 							</div>
@@ -48,5 +33,5 @@ export function SiteHeader() {
 				</div>
 			</div>
 		</header>
-	)
+	);
 }
