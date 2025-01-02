3e29f721c3053efe6065c0d1971c306ef77570a7
abirpahlwan <abirpahlwan@gmail.com>
2025-01-02 15:01:25 +0600

    indents

--- components/feature-card-center.tsx
+++ components/feature-card-center.tsx
@@ -5,13 +5,13 @@ import {Icons} from "@/components/icons";
 export function FeatureCardCenter() {
 	return (
 		<Card>
-            <CardContent className="flex flex-col items-center justify-center gap-4 p-8">
-              <Icons.heartpulse className="h-12 w-12 text-primary" />
-              <h3 className="text-xl font-bold">Healthcare</h3>
-              <p className="text-center text-muted-foreground">
-                Explore a wide range of healthcare services and information.
-              </p>
-            </CardContent>
-          </Card>
+			<CardContent className="flex flex-col items-center justify-center gap-4 p-8">
+				<Icons.brand className="h-12 w-12 text-primary" />
+				<h3 className="text-xl font-bold">Healthcare</h3>
+				<p className="text-center text-muted-foreground">
+				  Explore a wide range of healthcare services and information.
+				</p>
+			</CardContent>
+    	</Card>
 	);
 }
