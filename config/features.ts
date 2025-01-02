5fd14ccbf33434f34e3c624fe31256c356c85d9e
abirpahlwan <abirpahlwan@gmail.com>
2025-01-02 15:03:20 +0600

    features

---
+++ config/features.ts
@@ -0,0 +1,41 @@
+import {Icons} from "@/components/icons";
+
+export const features = [
+	{
+		title: "Brands",
+		href: "/brands",
+		description: "Medicine reference for students, doctors, pharmacists, nurses, and other healthcare professionals.",
+		icon: Icons.brand,
+		layout: 'center'
+	},
+	{
+		title: "Generics",
+		href: "/generics",
+		description: "Medicine reference for students, doctors, pharmacists, nurses, and other healthcare professionals.",
+		icon: Icons.generics
+	},
+	{
+		title: "Indications",
+		href: "/indications",
+		description: "Medicine reference for students, doctors, pharmacists, nurses, and other healthcare professionals.",
+		icon: Icons.indications
+	},
+	{
+		title: "Pharmaceuticals",
+		href: "/pharmaceuticals",
+		description: "Medicine reference for students, doctors, pharmacists, nurses, and other healthcare professionals.",
+		icon: Icons.pharmaceuticals
+	},
+	{
+		title: "Pharmacology",
+		href: "/pharmacology",
+		description: "Medicine reference for students, doctors, pharmacists, nurses, and other healthcare professionals.",
+		icon: Icons.pharmacology
+	},
+	{
+		title: "Therapeutic Classes",
+		href: "/classes",
+		description: "Medicine reference for students, doctors, pharmacists, nurses, and other healthcare professionals.",
+		icon: Icons.classes
+	},
+]
